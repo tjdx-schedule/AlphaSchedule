be3eee19bf07f7b75a97c0364183fff52ec41451
ZhangBowen <bwz96sco@outlook.com>
2026-01-15 15:45:03 +0800

    remove other

--- sci1-ppoV0.040/a2c_ppo_acktr/game/baselines_com/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 29 20:50:54 2019
-
-@author: luopeng
-"""
-
--- sci1-ppoV0.040/a2c_ppo_acktr/game/baselines_com/vec_env/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 29 20:51:34 2019
-
-@author: luopeng
-"""
-
